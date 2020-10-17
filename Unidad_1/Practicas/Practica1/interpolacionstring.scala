4e5604b1dc8d187e3afcf74a807dc63ac2c4e5de
Rebeca Ortiz <rebeca@rebeca>
2020-10-17 09:54:38 -0700

    exercise number 3 was performed

---
+++ Unidad_1/Practicas/Practica1/interpolacionstring.scala
@@ -0,0 +1,10 @@
+//Importamos la libreria con la cual trabajaremos 
+import scala.math
+
+//Se define el valor de la variable 
+val bird="tweet"
+
+//La "s" en cualquier cadena permite el uso de variables de manera directa dentro de la propia cadena 
+//"$" se anida dentro de la cadena procesada de tipo se
+
+println(s"Estoy escribiendo un, $bird")
